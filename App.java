45ab3dc80045baa204bcf7e73f7f401a85bc5354
Renne Castro <rcartist73@gmail.com>
2024-10-30 08:11:40 -0400

    specify width and height on execution

--- App.java
+++ App.java
@@ -4,8 +4,15 @@ public class App {
       public static JFrame frame;
       public static void main(String[] args) {
         frame = new JFrame();
+        String className = args[0];
+        int width = 800;
+        int height = 600; 
         //args[0] contains the class name that implements GameLogic in order to run the gameLoop()
-        Window c = new Window(800,600, args[0]);
+        if(args.length == 3){
+          width = Integer.parseInt(args[1]);
+          height = Integer.parseInt(args[2]);
+        }
+        Window c = new Window(width,height, className);
         frame.add(c);
         frame.setTitle(c.game.getTitle());
         frame.setSize(Game.width, Game.height);
