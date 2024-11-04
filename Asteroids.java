826b34a7394a102b2ead0bc7f217b8dfaca5345d
Renne Castro <rcartist73@gmail.com>
2024-11-04 05:24:50 -0500

    adds three levels

--- Asteroids.java
+++ Asteroids.java
@@ -6,13 +6,14 @@ public class Asteroids implements GameLogic{
     Animation plasmaball, explosion, crash;
     Shape healthBar, ammoBar;
     Font gameFont, basicFont;
+    int introCount = 0;
     
-    ArrayList<ArrayList<Animation>> asteroids = new ArrayList<>();
+    ArrayList<Animation> asteroids;
     ArrayList<Animation> energies;
     class Player{
-        public static int score = 0;
+        public static int asteroidsDestroyed = 0;
         public static int health = 100;
-        public static int ammo = 10;
+        public static int ammo = 20;
     }
 
     public Asteroids() {
@@ -23,18 +24,7 @@ public class Asteroids implements GameLogic{
         healthBar.moveTo(110,20);
         ammoBar = new Shape("rectangle",100,10,Color.ORANGE);
         ammoBar.moveTo(110, 50);
-        
-        asteroids.add(generateAsteroids(1, 5,2));//Level 1
-        asteroids.add(generateAsteroids(2, 10,4));//Level 2
-        asteroids.add(generateAsteroids(3, 5,2));//Level 3
-        
-        energies = new ArrayList<Animation>();
-        for(int i = 0; i < 5; i++){
-            Animation energy = new Animation("images/plasmaball3.png",5,60,60,2);
-            energy.x = Game.rnd(energy.width,Game.width-energy.width);
-            energy.y = Game.rnd(energy.height,Game.height-energy.height);
-            energies.add(energy);
-        }
+
         plasmaball = new Animation("images/plasmaball1.png",11,352/11,32,1);
         plasmaball.visible = false;
         explosion = new Animation("images/explosion.png",22,1254/22,64,0.30);
@@ -43,7 +33,7 @@ public class Asteroids implements GameLogic{
         crash.visible = false;
         ship = new Sprite("images/hero.gif");
 
-        gameFont = new Font("images/Arka_solid.ttf",100,Color.WHITE,Color.CYAN);
+        gameFont = new Font("images/battlestar.ttf",100,Color.WHITE,Color.CYAN);
         basicFont = new Font("Arial",40,Color.WHITE,Color.CYAN);
 
         Game.state = "startGame";
@@ -59,8 +49,14 @@ public class Asteroids implements GameLogic{
             case "startGame":
                 startGame();
                 break;
-            case "level1":
-                level1();
+            case "Level 1":
+                level(1);
+                break;
+            case "Level 2":
+                level(2);
+                break;
+            case "Level 3":
+                level3();
                 break;
         } 
     }
@@ -71,28 +67,33 @@ public class Asteroids implements GameLogic{
         Game.drawText("Press [SPACE] to start", 300,Game.height - 100,basicFont);
         ship.draw();
         if(Keys.pressed[Keys.SPACE]){
-            Game.state = "level1";
+            energies = generateEnergies(1,5);
+            asteroids = generateAsteroids(1, 5,2);
+            Game.state = "Level 1";
         }
     }
-    public void level1(){
+    public void level(int n){
         Game.drawBackground();
         healthBar.draw();
         ammoBar.draw();
 
         ship.move();
         crash.draw(false);
-
-        processAsteroids(asteroids.get(0));
-        
-        for(Animation energy:energies){
-            energy.draw();
-            if(ship.collidedWith(energy,"circle")){
-                energy.visible = false;
-                Player.ammo += 1;
-                ammoBar.width += 10;
-                ammoBar.x += 10;
-            }
+        if(introCount < 200){
+            Game.drawText(Game.state, 250, 200, gameFont);
+            introCount++;
+            return;
         }
+        if(Player.asteroidsDestroyed == asteroids.size()){
+            Player.asteroidsDestroyed = 0;
+            introCount = 0;
+            energies = generateEnergies(1,n * 4);
+            asteroids = generateAsteroids(n + 1, 5*(n+1),n + 2);
+            Game.state = "Level " + (n + 1);
+        }
+        processAsteroids(asteroids);
+        processEnergies();
+
         heroControl();
         plasmaball.move();
         if(plasmaball.isOffScreen()){
@@ -100,6 +101,74 @@ public class Asteroids implements GameLogic{
         }
         explosion.draw(false);
     }
+    public void level3(){
+        ArrayList<Animation> newAsteroids = new ArrayList<>();
+        Game.drawBackground();
+        healthBar.draw();
+        ammoBar.draw();
+
+        ship.move();
+        crash.draw(false);
+        if(introCount < 200){
+            Game.drawText(Game.state, 250, 200, gameFont);
+            introCount++;
+            return;
+        }
+        if(Player.asteroidsDestroyed == asteroids.size() * 3){
+            Game.state = "Game Over";
+        }
+        for(Animation asteroid: asteroids){
+            asteroid.move(true);
+            if(plasmaball.collidedWith(asteroid,"circle")){
+                asteroid.visible = false;
+                if((Integer)asteroid.get("hits") > 1){
+                    for(int i = 0; i < 2; i++){
+                        Animation a = new Animation("images/asteroid3.png",30, 510/6,500/5,0.5);
+                        a.resizeBy(-50);
+                        a.set("hits",1);
+                        int angle = Game.rnd(0,8) * 45 + 30;
+                        a.setSpeed(2, angle);
+                        a.x = asteroid.x;
+                        a.y = asteroid.y;
+                        newAsteroids.add(a);
+                    }
+                }
+                
+                plasmaball.visible = false;
+                explosion.visible = true;
+                explosion.moveTo(asteroid.x,asteroid.y);
+                Player.asteroidsDestroyed++;
+            }
+            if(asteroid.collidedWith(ship,"circle")){
+                asteroid.visible = false;
+                crash.visible = true;
+                crash.moveTo(ship.x,ship.y);
+                Player.asteroidsDestroyed++;
+                Player.health -= 10;
+                healthBar.width = Player.health;
+                healthBar.x -= 10;
+            }
+            for(Animation energy:energies){
+                if(asteroid.collidedWith(energy,"circle")){
+                    energy.visible = false;
+                    explosion.visible = true;
+                    explosion.moveTo(asteroid.x,asteroid.y);
+                }
+            }
+        }  
+        for(Animation a: newAsteroids){
+            asteroids.add(a);
+        }
+        processEnergies();
+
+        heroControl();
+        plasmaball.move();
+        if(plasmaball.isOffScreen()){
+            plasmaball.visible = false;
+        }
+        explosion.draw(false);
+    }
+
     public void heroControl(){
         if(Keys.pressed[Keys.UP]){
             ship.speed = 4;
@@ -118,8 +187,36 @@ public class Asteroids implements GameLogic{
             plasmaball.moveAngle = ship.moveAngle;
             plasmaball.speed = 6;
             Player.ammo -= 1;
-            ammoBar.width -= 10;
-            ammoBar.x -= 10;
+            ammoBar.width -= 5;
+            ammoBar.x -= 5;
+        }
+    }
+    public ArrayList<Animation> generateEnergies(int type, int amount){
+        ArrayList<Animation> energies = new ArrayList<>();
+        for(int i = 0; i < amount; i++){
+            Animation energy;
+            switch(type){
+                case 1:
+                energy = new Animation("images/plasmaball3.png",5,60,60,2);
+                    break;
+                default:
+                    energy = new Animation("images/plasmaball3.png",5,60,60,2);
+            }
+            energy.x = Game.rnd(energy.width,Game.width-energy.width);
+            energy.y = Game.rnd(energy.height,Game.height-energy.height);
+            energies.add(energy);
+        }
+        return energies;
+    }
+    public void processEnergies(){
+        for(Animation energy:energies){
+            energy.draw();
+            if(ship.collidedWith(energy,"circle")){
+                energy.visible = false;
+                Player.ammo += 1;
+                ammoBar.width += 10;
+                ammoBar.x += 10;
+            }
         }
     }
 
@@ -135,7 +232,8 @@ public class Asteroids implements GameLogic{
                     asteroid = new Animation("images/asteroid2.png",30, 960/30,32,1);
                     break;
                 case 3:
-                    asteroid = new Animation("images/asteroid3.png",30, 510/6,500/5,2);
+                    asteroid = new Animation("images/asteroid3.png",30, 510/6,500/5,0.5);
+                    asteroid.set("hits",2);
                     break;
                 default:
                     asteroid = new Animation("images/asteroid1t.gif",41, 2173/41,52,1); 
@@ -157,12 +255,13 @@ public class Asteroids implements GameLogic{
                 plasmaball.visible = false;
                 explosion.visible = true;
                 explosion.moveTo(asteroid.x,asteroid.y);
-                Player.score += 10;
+                Player.asteroidsDestroyed++;
             }
             if(asteroid.collidedWith(ship,"circle")){
                 asteroid.visible = false;
                 crash.visible = true;
                 crash.moveTo(ship.x,ship.y);
+                Player.asteroidsDestroyed++;
                 Player.health -= 10;
                 healthBar.width = Player.health;
                 healthBar.x -= 10;
